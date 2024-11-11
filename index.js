41e38bf5822e0321965d6deaeada81d02aae9a86
Md-Emran-Hossen <emran.cse21@gmail.com>
2024-11-11 20:44:28 +0600

    Components update successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 
@@ -30,6 +30,8 @@ async function run() {
     const projectCollection = client.db("myPortfolio").collection("projects");
     const technologyCollection = client.db("myPortfolio").collection("technologies");
     const serviceCollection = client.db("myPortfolio").collection("services");
+    const educationCollection = client.db("myPortfolio").collection("educations");
+    const experienceCollection = client.db("myPortfolio").collection("experiences");
     
       // project route
       app.post("/projects", async (req, res) => {
@@ -45,6 +47,45 @@ async function run() {
         res.send(result);
       });
 
+      app.delete("/project/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await projectCollection.deleteOne(query);
+        res.send(result);
+      });
+  
+      app.get("/project/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await projectCollection.findOne(query);
+        res.send(result);
+      });
+  
+      app.put("/project/:id", async (req, res) => {
+        const pId = req.params.id;
+        console.log("Update Data Found",pId);
+        const project = req.body;
+        const filter = { _id: new ObjectId(pId) };
+        const option = { upsert: true };
+        
+        const updatedProject = {
+          $set: {
+            projectName: project.projectName,
+            projectLiveUrl: project.projectLiveUrl,
+            gitHubUrl: project.gitHubUrl,
+            description: project.description,
+          },
+        };
+
+        const result = await projectCollection.updateOne(
+          filter,
+          updatedProject,
+          option
+        );
+        res.send(result);
+      });
+  
+
       // technology route
       app.post("/technologies", async (req, res) => {
         const technology = req.body;
@@ -56,6 +97,13 @@ async function run() {
       app.get("/technologies", async (req, res) => {
         const query = technologyCollection.find();
         const result = await query.toArray();
+        res.send(result);
+      });
+
+      app.delete("/technology/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await technologyCollection.deleteOne(query);
         res.send(result);
       });
 
@@ -72,6 +120,57 @@ async function run() {
         const result = await query.toArray();
         res.send(result);
       });
+
+      app.delete("/service/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await serviceCollection.deleteOne(query);
+        res.send(result);
+      });
+
+      // education section
+      app.post("/educations", async (req, res) => {
+        const educations = req.body;
+        const result = await educationCollection.insertOne(educations);
+        console.log("Education Info: ", result);
+        res.send(result);
+      });
+
+      app.get("/educations", async (req, res) => {
+        const query = educationCollection.find();
+        const result = await query.toArray();
+        res.send(result);
+      });
+
+      app.delete("/education/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await educationCollection.deleteOne(query);
+        res.send(result);
+      });
+
+       // experience section
+       app.post("/experiences", async (req, res) => {
+        const experience = req.body;
+        const result = await experienceCollection.insertOne(experience);
+        console.log("Experience Info: ", result);
+        res.send(result);
+      });
+
+      app.get("/experiences", async (req, res) => {
+        const query = experienceCollection.find();
+        const result = await query.toArray();
+        res.send(result);
+      });
+
+      app.delete("/experience/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await experienceCollection.deleteOne(query);
+        res.send(result);
+      });
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
